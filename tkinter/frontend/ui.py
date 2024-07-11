3a9729cf52d2737e6d9f8211bd80c01352cbf305
jetson <iroo200121@gmail.com>
2024-07-12 00:15:17 +0900

    MOD ui.py

--- tkinter/frontend/ui.py
+++ tkinter/frontend/ui.py
@@ -1,3 +1,8 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from backend import main
+backend = main.main()
+
 import tkinter as tk
 import tkinter.font as tkfont
 from tkinter import ttk, messagebox
