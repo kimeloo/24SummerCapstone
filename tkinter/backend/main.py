4af1d2c2158afff3006e995f6168178365c77d19
jetson <iroo200121@gmail.com>
2024-07-12 00:12:06 +0900

    ADD ui.py

---
+++ tkinter/backend/main.py
@@ -0,0 +1,9 @@
+class main():
+    def __init__(self):
+        print("Init")
+    
+    def from_ui(self, key, value):
+        print(f'{key} : {value}')
+
+if __name__ == '__main__':
+    main()
---
+++ tkinter/main.py
@@ -0,0 +1,3 @@
+import os
+print(os.chdir('./src/'))
+from frontend import ui
