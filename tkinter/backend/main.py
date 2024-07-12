91a7ab3292a1fa43f0278c83b9adf336d6b5ff37
jetson <iroo200121@gmail.com>
2024-07-13 03:31:06 +0900

    FEAT : backend handler 기능 추가
    user_id 저장
    UI에서 넘어온 데이터 처리 및 반환

--- tkinter/backend/main.py
+++ tkinter/backend/main.py
@@ -11,5 +11,14 @@ class connectUI():
     def buttonOnClick(self, _from):
         print(f'buttonOnClick from {_from}')
 
+class handler():
+    '''frontend에서 넘어온 데이터 처리 및 요구 데이터 반환'''
+    def __init__(self):
+        self.user_id = None
+    
+    
+
+    # def 
+
 if __name__ == '__main__':
     print('Do not run this file directly.')
