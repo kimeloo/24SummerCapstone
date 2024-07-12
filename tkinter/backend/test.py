df41a0845b9b75b5517fed93073027504ad18810
jetson <iroo200121@gmail.com>
2024-07-12 19:05:06 +0900

    TEST : backend DB SELECT & INSERT 테스트 코드 작성
    useraccount에서 ID SELECT
    ID와 physical val 일부 INSERT TEST
    INSERT 내용 전체 SELECT로 조회 TEST
    테스트 성공

--- tkinter/backend/test.py
+++ tkinter/backend/test.py
@@ -4,6 +4,31 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from backend.main import connectDB
 
 dbTest = connectDB()
-where = dict()
-where['user_id'] = '1'
-print(dbTest._selectTb('details', ['Sex', 'Bmi', 'Fat'], where, latest=0))
+
+# # SELECT Test
+# where = dict()
+# where['user_id'] = '1'
+# print(dbTest._selectTb('details', ['Sex', 'Bmi', 'Fat'], where, latest=0))
+
+# INSERT Test
+data = dict()
+data['나이'] = 9
+data['성별'] = 'Female'
+data['키 (cm)'] = 9
+data['체중 (kg)'] = 9
+data['체지량지수 (BMI)'] = 9
+data['체지방 (Kg)'] = 9
+print(data)
+## 실제 전화번호 형식은 '-' 없음, test dummy에는 - 존재
+(1, 'John Doe', 'john.doe@example.com', '010-1234-5678'),
+selectByPhone = dict()
+selectByPhone['phone_number'] = '010-1234-5678'
+userID = dbTest._selectTb('useraccount', ['ID'], selectByPhone, isWhereDb=False)[0][0]
+print(userID)
+
+selectByID = dict()
+selectByID['user_id'] = userID
+data['user_id'] = userID
+print(data)
+dbTest._insertTb('details', data)
+print(dbTest._selectTb('details', ['*'], selectByID))
