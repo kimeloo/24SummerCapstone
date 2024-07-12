d2d06758830d93ad07b47a88f4d201773e4df0bd
radihy <soiyosauce@gmail.com>
2024-07-12 23:27:29 +0900

    MOD : admin_page에 mariadb 내용 띄우기

--- django/admin_page/views.py
+++ django/admin_page/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from .models import UserAccount, Details, Health, Sensors
 
-def index(request):
-    content_list = UserAccount.objects.all()
-    # content_list = UserAccount.objects.order_by('-pub_date')[:5] # 역순 정렬(최신 콘텐츠 상단에 노출)
-    context = {'content_list': content_list}
+def combined_view(request):
+    users = UserAccount.objects.all()
+    details = Details.objects.all()
+    health = Health.objects.all()
+    sensors = Sensors.objects.all()
+    context = {
+        'users': users,
+        'details': details,
+        'health': health,
+        'sensors': sensors,
+    }
     return render(request, 'admin_page/content_list.html', context)
