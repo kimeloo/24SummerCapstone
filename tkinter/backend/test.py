9b7971ad116754d440c3e9566f6e522eecb46c37
jetson <iroo200121@gmail.com>
2024-07-13 23:21:24 +0900

    TEST : backend DB 리팩토링 이후 테스트

--- tkinter/backend/test.py
+++ tkinter/backend/test.py
@@ -6,36 +6,36 @@ from backend.connectDB import ConnectDB
 
 dbTest = ConnectDB()
 
-# ##### SELECT Test #####
-# where = dict()
-# where['user_id'] = '1'
-# print(dbTest._selectTb('details', ['Sex', 'Bmi', 'Fat'], where, latest=1))
+##### SELECT Test #####
+where = dict()
+where['user_id'] = '1'
+print(dbTest._selectTb('details', ['Sex', 'Bmi', 'Fat'], where, latest=1))
 
-# ##### INSERT Test #####
-# data = dict()
-# input_data = list(input('data 입력\n 성별, 키, 체중, 체질량지수, 체지방 >>').split())
-# keys = ['성별', '키 (cm)', '체중 (kg)', '체지량지수 (BMI)', '체지방 (Kg)']
-# for key, val in zip(keys, input_data):
-#     data[key] = val
-# print('\n입력 데이터 : ')
+##### INSERT Test #####
+data = dict()
+input_data = list(input('data 입력\n 성별, 키, 체중, 체질량지수, 체지방, user_id >>').split())
+keys = ['성별', '키 (cm)', '체중 (kg)', '체지량지수 (BMI)', '체지방 (Kg)', 'user_id']
+for key, val in zip(keys, input_data):
+    data[key] = val
+print('\n입력 데이터 : ')
+print(data)
+# (1, 'John Doe', 'john.doe@example.com', '01012345678'),
+selectByPhone = dict()
+
+selectByPhone['phone_number'] = input('phone_number 입력 >> ')
+userID = dbTest._selectTb('useraccount', ['ID'], selectByPhone, isWhereDb=False)[0]['ID']
+print('\nuserID : ')
+print(userID)
+
+selectByID = dict()
+selectByID['user_id'] = userID
+data['user_id'] = userID
 # print(data)
-# # (1, 'John Doe', 'john.doe@example.com', '01012345678'),
-# selectByPhone = dict()
+dbTest._insertTb('details', data)
+print('\n입력 후 조회된 데이터 : ')
+print(dbTest._selectTb('details', ['*'], selectByID))
 
-# selectByPhone['phone_number'] = input('phone_number 입력 >> ')
-# userID = dbTest._selectTb('useraccount', ['ID'], selectByPhone, isWhereDb=False)[0]['ID']
-# print('\nuserID : ')
-# print(userID)
-
-# selectByID = dict()
-# selectByID['user_id'] = userID
-# data['user_id'] = userID
-# # print(data)
-# dbTest._insertTb('details', data)
-# print('\n입력 후 조회된 데이터 : ')
-# print(dbTest._selectTb('details', ['*'], selectByID))
-
-# input()
+input()
 ##### UPDATE Test #####
 data = dict()
 input_data = list(input('data 입력\n 나이, 성별, 키, 체중, 체질량지수, 체지방 >>').split())
