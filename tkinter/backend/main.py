b97151292e1a67c27735fe04e5a0cdc131a113db
kimeloo <iroo200121@gmail.com>
2024-07-14 00:29:28 +0900

    RESET dev

--- tkinter/backend/main.py
+++ tkinter/backend/main.py
@@ -1,9 +1,10 @@
-from . import connectDB
+import requests
 
 class ConnectUI():
     '''frontend UI/UX와 연결'''
     def __init__(self):
-        self.db = connectDB.ConnectDB()     # DB 커넥션 우선 확인
+        '''Server 연결'''
+        self.server = ConnectServer()
     
     def getValue(self, key, value):
         '''frontend에서 호출하여, 변수명과 값을 backend로 전달'''
@@ -13,10 +14,10 @@ class ConnectUI():
         '''frontend에서 호출하여, 버튼의 기능과 페이지 위치를 backend로 전달'''
         print(f'{which} buttonOnClick from {_from}')
 
-class Handler():
+class ConnectServer():
     '''frontend에서 넘어온 데이터 처리 및 요구 데이터 반환'''
     def __init__(self):
-        self.user_id = None                 # user_id 값을 저장하여 DB 상호작용에서 활용
+        print(None)
     
     
 
