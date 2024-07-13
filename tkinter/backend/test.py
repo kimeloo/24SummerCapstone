fc74c2ce8be56e47a6258eacb731781919b58948
jetson <iroo200121@gmail.com>
2024-07-13 11:29:46 +0900

    TEST : backend DB UPDATE 테스트 코드 작성
    테스트 및 오류 수정

--- tkinter/backend/test.py
+++ tkinter/backend/test.py
@@ -6,12 +6,37 @@ from backend.connectDB import connectDB
 
 dbTest = connectDB()
 
-# # SELECT Test
+# ##### SELECT Test #####
 # where = dict()
 # where['user_id'] = '1'
 # print(dbTest._selectTb('details', ['Sex', 'Bmi', 'Fat'], where, latest=0))
 
-# INSERT Test
+# ##### INSERT Test #####
+# data = dict()
+# input_data = list(input('data 입력\n 성별, 키, 체중, 체질량지수, 체지방 >>').split())
+# keys = ['성별', '키 (cm)', '체중 (kg)', '체지량지수 (BMI)', '체지방 (Kg)']
+# for key, val in zip(keys, input_data):
+#     data[key] = val
+# print('\n입력 데이터 : ')
+# print(data)
+# # (1, 'John Doe', 'john.doe@example.com', '01012345678'),
+# selectByPhone = dict()
+
+# selectByPhone['phone_number'] = input('phone_number 입력 >> ')
+# userID = dbTest._selectTb('useraccount', ['ID'], selectByPhone, isWhereDb=False)[0][0]
+# print('\nuserID : ')
+# print(userID)
+
+# selectByID = dict()
+# selectByID['user_id'] = userID
+# data['user_id'] = userID
+# # print(data)
+# dbTest._insertTb('details', data)
+# print('\n입력 후 조회된 데이터 : ')
+# print(dbTest._selectTb('details', ['*'], selectByID))
+
+# input()
+# ##### UPDATE Test #####
 data = dict()
 input_data = list(input('data 입력\n 나이, 성별, 키, 체중, 체질량지수, 체지방 >>').split())
 keys = ['나이', '성별', '키 (cm)', '체중 (kg)', '체지량지수 (BMI)', '체지방 (Kg)']
@@ -27,10 +52,4 @@ userID = dbTest._selectTb('useraccount', ['ID'], selectByPhone, isWhereDb=False)
 print('\nuserID : ')
 print(userID)
 
-selectByID = dict()
-selectByID['user_id'] = userID
-data['user_id'] = userID
-# print(data)
-dbTest._insertTb('details', data)
-print('\n입력 후 조회된 데이터 : ')
-print(dbTest._selectTb('details', ['*'], selectByID))
+print(f'UPDATE success : {dbTest._updateTb("details", data, userID)}')
