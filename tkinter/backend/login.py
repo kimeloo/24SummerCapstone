b80067a331d1efb3c6af99c5dfb31a4f3b7107cb
gahyeon <soiyosauce@gmail.com>
2024-07-16 00:57:49 +0900

    FEAT : CLIENT UI&DB connect 기능 추가
    main.py에 TEST코드 포함

--- tkinter/backend/login.py
+++ tkinter/backend/login.py
@@ -1,5 +1,4 @@
 # login 로직 (클라이언트에서 jwt를 사용, Django에 연동할 거임, id만을 이용해서)
-
 import requests
 import os, sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
@@ -7,25 +6,24 @@ from envLoaderServer import loadEnv
 SERVER_HOST = loadEnv()
 
 class JwtAuthClient:
-    def __init__(self, server_url):
-        self.server_url = server_url
-        self.login_endpoint = '/api/login/'
-        self.jwt_token = None
+    def __init__(self, serverUrl):
+        self.serverUrl = serverUrl
+        self.Endpoint = '/api/login/'
+        self.token = None
 
     # 사용자 ID를 전달하여 로그인 요청을 보냄
-    def login(self, user_id):
+    def login(self, phone_number=None, email_address=None):
         data = {
-            'id': user_id
+            'phone_num' : phone_number,
+            'email' : email_address
         }
-
         try:
-            response = requests.post(self.server_url + self.login_endpoint, json=data)
+            response = requests.post(self.serverUrl + self.Endpoint, data=data)
             response_data = response.json()
-
             if response.status_code == 200:
                 # 로그인 요청 결과로 받은 JWT 토큰은 클래스 내의 jwt_token 속성에 저장됨
-                self.jwt_token = response_data.get('token') # 여기에 토큰을 입력
-                self.user_id = response_data.get('ID') # 여기에 ID를 입력
+                self.jwt_token = response_data.get('token')
+                self.user_id = response_data.get('ID')
                 print("로그인 성공!")
             else:
                 print("로그인 실패:", response_data.get('message'))
@@ -39,7 +37,8 @@ class JwtAuthClient:
 # 클래스 테스트 코드
 if __name__ == "__main__":
     client = JwtAuthClient(SERVER_HOST)
-    client.login('1')
-    # 현재 저장된 JWT 토큰 가져옴
-    token = client.get_token()
-    print("받은 JWT 토큰 :", token)
+    phone_number = input('전화번호 입력 >> ')
+    # email = input('email 입력 >> ')
+    client.login(phone_number=phone_number)
+    # client.login(email=email)
+    print("받은 JWT 토큰 :", client.get_token())
