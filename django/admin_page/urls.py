b269b29af18e1ac0d3cd913fe899cf85827db11e
radihy <soiyosauce@gmail.com>
2024-07-17 05:30:42 +0900

    MOD : 최종수정

--- django/admin_page/urls.py
+++ django/admin_page/urls.py
@@ -1,22 +1,6 @@
-"""
-URL configuration for capstone_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 from .views import combined_view
 
 urlpatterns = [
-    path('', combined_view, name='combined_view'), #127.0.0.1/admin_page에 들어오면 combined_view함수를 실행.
+    path('', combined_view), #127.0.0.1/admin_page에 들어오면 combined_view함수를 실행.
 ]
--- django/admin_page/views.py
+++ django/admin_page/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from .models import UserAccount, Details, Health, Sensors, Recommend
 
 def combined_view(request):
-    users = UserAccount.objects.all()
-    details = Details.objects.all()
-    health = Health.objects.all()
-    sensors = Sensors.objects.all()
-    recommend = Recommend.objects.all()
+    users = UserAccount.objects.all().reverse()[:5]
+    details = Details.objects.all().reverse()[:5]
+    health = Health.objects.all().reverse()[:5]
+    sensors = Sensors.objects.all().reverse()[:5]
+    recommend = Recommend.objects.all().reverse()[:5]
     context = {
         'users': users,
         'details': details,
